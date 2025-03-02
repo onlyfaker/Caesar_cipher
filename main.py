9e63345e22c05bfcf5a807da7849ef11fea74443
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-01 20:42:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 
 def encode(msg1,shift_num1):
-    ascii_values = [ord(char) for char in msg1]
-    length_for_iteration = len(ascii_values)
-    for vall in range(length_for_iteration):
-        ascii_values[vall] += shift_num1
+    ascii_values = []
+    for vall in msg1:
+        if vall == ' ':  # If the character is a space
+            ascii_values.append(ord(vall))  # Add space without shifting
+        else:
+            ascii_values.append(ord(vall) + shift_num1)  # Shift non-space characters
     characters = [chr(value) for value in ascii_values]
     print(''.join(characters))
 
 def decode(msg2,shift_num2):
-    ascii_values = [ord(char) for char in msg2]
-    length_for_iteration = len(ascii_values)
-    for vall in range(length_for_iteration):
-        ascii_values[vall] -= shift_num2
+    ascii_values = []
+    for char in msg2:
+        if char == ' ':  #If the character is a space
+            ascii_values.append(ord(char))  # Add space without shifting
+        else:
+            ascii_values.append(ord(char) - shift_num2)  # Shift non-space characters
     characters = [chr(value) for value in ascii_values]
     print(''.join(characters))
 
-#todo - make it so that it doesnt count spaces in ascii
 again='yes'
 while(again=='yes'):
-    user_input = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
-    if user_input=='encode':
-        msg1 = input('Type your message: ')
-        shift_num1=int(input('Type the shift number: '))
-        encode(msg1,shift_num1)
+    # Get and validate user operation choice
+    while True:
+        user_input = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+        if user_input == 'encode' or user_input == 'decode':
+            break
+        else:
+            print("Invalid input. Please type 'encode' or 'decode'!!!")
 
-    if user_input=='decode':
+    # Process based on validated choice
+    if user_input == 'encode':
+        msg1 = input('Type your message: ')
+        shift_num1 = int(input('Type the shift number: '))
+        encode(msg1, shift_num1)
+
+    else:  # user_input must be 'decode' here
         msg2 = input('Type your message: ')
-        shift_num2=int(input('Type the shift number: '))
-        decode(msg2,shift_num2)
-    again = input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+        shift_num2 = int(input('Type the shift number: '))
+        decode(msg2, shift_num2)
+
+    # Get and validate user's choice to continue
+    while True:
+        again = input("Type 'yes' if you want to go again. Otherwise type 'no': ").lower()
+        if again == 'yes' or again == 'no':
+            break
+        else:
+            print("Invalid input. Please type 'yes' or 'no': ")
+
 
 
 
